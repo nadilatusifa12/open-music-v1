c19cc55f956718277ae5e1b40aa367724e2e57c6
nadilatusifa <nadilatusifa83@gmail.com>
2023-07-18 08:07:05 +0700

    ganti duration dan albumId song jadi boleh null

--- migrations/1689584593266_create-table-song.js
+++ migrations/1689584593266_create-table-song.js
@@ -26,11 +26,11 @@ exports.up = pgm => {
       },
       duration: {
         type: "INTEGER",
-        notNull: true,
+        notNull: false,
       },
       albumId: {
         type: "VARCHAR(50)",
-        notNull: true,
+        notNull: false,
       },
       create_at: {
         type: "TEXT",
